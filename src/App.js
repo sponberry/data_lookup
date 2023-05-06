5c92c64409dc0030afbefb69ad8549e0a844bc88
Abi Nottingham <abigailillustration@gmail.com>
2023-05-06 16:19:29 +0100

    adds ApolloClient and basic request on app load

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import './App.css';
+import { useEffect } from 'react';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
 
 function App() {
+  
+
+  useEffect(() => {
+    const query = gql`
+      query {
+        allCustomers  {
+          email,
+          forename,
+          surname,
+          contact_number,
+          postcode,
+        }
+      }
+    `
+    
+    client.query({ query })
+      .then((response) => {
+        console.log(response.data)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Initial app setup</p>
     </div>
   );
 }
