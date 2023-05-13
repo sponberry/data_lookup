c87fa3cbb85e715991a90ee0b35346e5c7f8d132
Abi Nottingham <abigailillustration@gmail.com>
2023-05-13 15:58:16 +0100

    creates basic buttons to change application page

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import CustomerList from "./Customer List";
 import ProductList from "./Product List";
 
-import {
-  BrowserRouter as Router,
-  //Route, Link, Switch
-} from "react-router-dom";
-
 function App() {
-  {
-    /* <Router>
-        <Container>
-          <Header as="h1">Patientor</Header>
-          <Button as={Link} to="/" primary>
-            Home
-          </Button>
-          <Divider hidden />
-          <Switch>
-            <Route path="/patients/:id">
-              <PatientViewPage />
-            </Route>
-            <Route path="/">
-              <PatientListPage />
-            </Route>
-          </Switch>
-        </Container>
-      </Router> */
-  }
-  return (
-    <div className='App'>
-      <Router>
+  const [page, setPage] = useState("/");
+  if (page === "customers") {
+    return (
+      <div className='App'>
+        <button onClick={() => setPage("/")}>Go back</button>
         <CustomerList />
+      </div>
+    );
+  } else if (page === "products") {
+    return (
+      <div className='App'>
+        <button onClick={() => setPage("/")}>Go back</button>
         <ProductList />
-      </Router>
-    </div>
-  );
+      </div>
+    );
+  } else {
+    return (
+      <div className='App'>
+        <button onClick={() => setPage("customers")}>Customers</button>
+        <button onClick={() => setPage("products")}>Products</button>
+      </div>
+    );
+  }
 }
 
 export default App;
